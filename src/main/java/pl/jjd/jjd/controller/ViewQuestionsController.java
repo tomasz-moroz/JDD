4360b1eaf36b871965f04da210339bdc5357c2bf
TMO <morotmk@gmail.com>
2021-01-19 15:08:44 +0100

    Nice looking question list added (with pagination). Missing buttons with redirect to the following pages

--- src/main/java/pl/jjd/jjd/controller/ViewQuestionsController.java
+++ src/main/java/pl/jjd/jjd/controller/ViewQuestionsController.java
@@ -3,11 +3,14 @@ package pl.jjd.jjd.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.jjd.jjd.dto.QuestionDto;
 import pl.jjd.jjd.service.QuestionService;
+
+import java.util.*;
 
 @Controller
 public class ViewQuestionsController {
@@ -20,7 +23,13 @@ public class ViewQuestionsController {
 
     @GetMapping
     @RequestMapping(path = "/viewQuestions")
-    public  String getList(){
+    public String getViewQuestions(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "25") Integer size, Model model) {
+        Page<QuestionDto> questionDtoPage = questionService.findAll(PageRequest.of(page,size));
+        List<QuestionDto> questionDtoList = questionDtoPage.getContent();
+        model.addAttribute("questionList", questionDtoList);
+        model.addAttribute("page", page);
+        model.addAttribute("totalPages", questionDtoPage.getTotalPages());
+        model.addAttribute("totalItems", questionDtoPage.getTotalElements());
         return "viewQuestions";
     }
 }
