fff50afac361f9bd08932d59c878a0110ffd1beb
TMO <morotmk@gmail.com>
2021-01-21 13:31:29 +0100

    Added delete/update options in view questions template, but updete doesnt work

--- src/main/java/pl/jjd/jjd/controller/ViewQuestionsController.java
+++ src/main/java/pl/jjd/jjd/controller/ViewQuestionsController.java
@@ -1,10 +1,12 @@
 package pl.jjd.jjd.controller;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.jjd.jjd.dto.QuestionDto;
 import pl.jjd.jjd.service.QuestionService;
@@ -42,9 +44,26 @@ public class ViewQuestionsController {
         return "viewQuestions";
     }
 
-    @GetMapping(path ="/viewQuestions/")
+    @GetMapping(path = "/viewQuestions/")
     public String viewListOfQuestions(Model model) {
-       return findPaginated(1, model);
+        return findPaginated(1, model);
+    }
+    @GetMapping(path = "/deleteQuestion/{id}")
+    public String deleteQuestion(@PathVariable(value = "id") long id) {
+        questionService.delete(id);
+        return "redirect:/viewQuestions/0";
+    }
+
+
+
+
+    @PostMapping(path = "/showFormForUpdate/{id}")
+    public String updateQuestion(@PathVariable(value = "id") long id, Model model, QuestionDto questionDto) throws ChangeSetPersister.NotFoundException {
+        model.addAttribute("question", questionDto.getQuestion());
+        model.addAttribute("answer", questionDto.getQuestion());
+        model.addAttribute("category", questionDto.getCategory());
+        questionService.edit(id,questionDto);
+        return "redirect:/viewQuestions/0";
     }
 
 }
